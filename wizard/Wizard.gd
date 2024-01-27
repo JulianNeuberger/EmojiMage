19e72f70c6f53a165b75106bd2b8064cbebda984
Johannes König <koenig.nexus@gmail.com>
2024-01-27 15:04:19 +0100

    add health bar for player and connect to health component

--- wizard/Wizard.gd
+++ wizard/Wizard.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
+class_name Wizard
 
 @export var movement_parameters: MovementResource
+@onready var health = $Health
+
 
 func _process(delta):
 	velocity *= movement_parameters.movement_speed
