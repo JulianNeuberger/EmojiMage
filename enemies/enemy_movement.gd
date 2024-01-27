8a40ce8f6a24408794e1e9e70a91659ee7374745
Julian Neuberger <julian.neuberger@uni-bayreuth.de>
2024-01-27 20:20:29 +0100

    wip on movement

--- enemies/enemy_movement.gd
+++ enemies/enemy_movement.gd
@@ -6,6 +6,7 @@ class_name EnemyMovement
 @onready var parent_character_2d: CharacterBody2D = get_parent()
 
 var _nav_agent: NavigationAgent2D = null
+var _movement_delta: float
 
 
 func _physics_process(delta):
@@ -17,16 +18,30 @@ func _physics_process(delta):
 	
 	var diff = next_position - position
 	var velocity = diff.normalized() * movement_per_sec
+	
+	_movement_delta = delta
 
-	parent_character_2d.velocity = velocity * movement_per_sec
-	var collision = parent_character_2d.move_and_collide(parent_character_2d.velocity * delta)
-	if collision:
-		parent_character_2d.velocity = parent_character_2d.velocity.slide(collision.get_normal())
+	var new_velocity = velocity * movement_per_sec
+	if _nav_agent.avoidance_enabled:
+		_nav_agent.set_velocity(new_velocity)
+	else:
+		_on_velocity_computed(new_velocity)
 
 func set_nav_agent(nav_agent: NavigationAgent2D):
 	_nav_agent = nav_agent
+	if _nav_agent == null:
+		return
+	_nav_agent.connect("velocity_computed", _on_velocity_computed)
 
 func set_target(target: Vector2):
 	if _nav_agent == null: 
 		return
 	_nav_agent.target_position = target
+	
+func _on_velocity_computed(safe_velocity: Vector2) -> void:
+	parent_character_2d.velocity = safe_velocity# * _movement_delta
+	var collision = parent_character_2d.move_and_slide()
+	#if collision:
+	#	parent_character_2d.velocity = parent_character_2d.velocity.slide(collision.get_normal())
+	
+	#global_position = parent_character_2d.global_position.move_toward(parent_character_2d.global_position + safe_velocity, _movement_delta)
