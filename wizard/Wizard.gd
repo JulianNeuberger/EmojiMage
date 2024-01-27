9cdd19d65469e3d7f7e9bc8d063ca73e5e63e7b7
Johannes König <koenig.nexus@gmail.com>
2024-01-27 21:27:52 +0100

    add level change

--- wizard/Wizard.gd
+++ wizard/Wizard.gd
@@ -10,11 +10,15 @@ func _ready():
 	inventory.init()
 	$PlayerInventoryHandler.connect("inventory_left", inventory.scroll_slot_left)
 	$PlayerInventoryHandler.connect("inventory_right", inventory.scroll_slot_right)
-	inventory.connect("slot_changed", func(_slot_index, slot_value): $Wand.set_wand_resorce(slot_value))
+	inventory.connect("slot_changed", func(_slot_index, slot_value): set_wand(slot_value))
+	set_wand(inventory.slots[0])
 
 func _process(delta):
 	velocity *= movement_parameters.movement_speed
 	move_and_slide()
+
+func set_wand(wand_resource: WandResource):
+	$Wand.set_wand_resorce(wand_resource)
 
 func serialize() -> Dictionary:
 	var dict = {
